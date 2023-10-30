2279d7ea2c77461f66821c0700824183b241e1b0
Hemanth Prabhu <hemanthprabhu056@gmail.com>
2023-10-30 16:19:41 +0530

    Update mergesort.py
    updated

--- mergesort.py
+++ mergesort.py
@@ -1,100 +1,18 @@
-// Java program for Merge Sort
-import java.io.*;
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-class MergeSort {
+    mid = len(arr) // 2
+    left, right = arr[:mid], arr[mid:]
 
-	// Merges two subarrays of arr[].
-	// First subarray is arr[l..m]
-	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r)
-	{
-		// Find sizes of two subarrays to be merged
-		int n1 = m - l + 1;
-		int n2 = r - m;
+    merge = lambda l, r: [l.pop(0) if l[0] < r[0] else r.pop(0) for _ in range(len(l) + len(r))]
+    
+    return merge(merge_sort(left), merge_sort(right))
 
-		// Create temp arrays
-		int L[] = new int[n1];
-		int R[] = new int[n2];
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    sorted_arr = merge_sort(arr)
+    print("Sorted array:", sorted_arr)
 
-		// Copy data to temp arrays
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
 
-		// Merge the temp arrays
-
-		// Initial indices of first and second subarrays
-		int i = 0, j = 0;
-
-		// Initial index of merged subarray array
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			}
-			else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
-
-		// Copy remaining elements of L[] if any
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
-
-		// Copy remaining elements of R[] if any
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
-	}
-
-	// Main function that sorts arr[l..r] using
-	// merge()
-	void sort(int arr[], int l, int r)
-	{
-		if (l < r) {
-
-			// Find the middle point
-			int m = l + (r - l) / 2;
-
-			// Sort first and second halves
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
-
-			// Merge the sorted halves
-			merge(arr, l, m, r);
-		}
-	}
-
-	// A utility function to print array of size n
-	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver code
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-		System.out.println("Given array is");
-		printArray(arr);
-
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
-
-		System.out.println("\nSorted array is");
-		printArray(arr);
-	}
-}
+/* This code is contributed by Hemanth Prabhu */
